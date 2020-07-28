851d70b2de1d3aae645c5e9f7e3ebaf64ca9a372
BillyNight <jvrrebello@gmail.com>
2020-07-28 00:31:20 -0300

    Criado o objeto player
    Script de movimentação também foi criado;

---
+++ Survival Game/Assets/Scripts/playerMov.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMov : MonoBehaviour
+{
+    public CharacterController controlador;
+    public Camera mainCam;
+    float velocity = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        mainCam.transform.position = new Vector3 (transform.position.x, transform.position.y, -10f);
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 movimento = Vector3.Normalize(transform.up * y + transform.right * x);
+        controlador.Move(movimento * velocity * Time.deltaTime);
+    }
+}
